e416cd2c5c9503927fb80c399dbe2239933a1b72
ji <1162568294@qq.com>
2025-05-18 13:53:16 +0800

    代码初始化

---
+++ src/main/java/com/ji/open/JiOpenApplication.java
@@ -0,0 +1,13 @@
+package com.ji.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiOpenApplication.class, args);
+	}
+
+}
